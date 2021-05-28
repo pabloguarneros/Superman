e3ad96ae696b313f4288fac1603f4a3671aa3ecf
pabloguarneros <wrno.pablo@gmail.com>
2021-05-28 10:05:47 -0400

    Added requirements and first migration

--- user_dashboard/models.py
+++ user_dashboard/models.py
@@ -1,3 +1,10 @@
+from PIL import Image
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+def profile_pic_directory_path(instance, filename): 
+    return 'users/{0}/profile_pic/{1}'.format(instance.username, f"{filename}")  
+
+class User(AbstractUser):
+    email = models.EmailField('email', unique=True)
+    profile_pic = models.ImageField(upload_to=profile_pic_directory_path, blank=True)
